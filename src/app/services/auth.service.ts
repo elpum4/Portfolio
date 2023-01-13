fbc52e338d49caf15c40f4239fcf8b46422f9aa1
elpum4 <franckocer@gmail.com>
2023-01-13 14:27:50 -0300

    change signin

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,48 +1,53 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   //url = 'http://localhost:8080/api/auth';
-  url = 'https://backapp-elpum4.herokuapp.com/api/auth';
+  url = 'https://backapi-agub.onrender.com/api/auth';
 
   currentUserSubject: BehaviorSubject<any>;
 
   constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<any>
-    (JSON.parse(sessionStorage.getItem('currentUser')||'{}'));
-
-   }
-  getOptions(){
+    this.currentUserSubject = new BehaviorSubject<any>(
+      JSON.parse(sessionStorage.getItem('currentUser') || '{}')
+    );
+  }
+  getOptions() {
     const valores = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
+        'Content-Type': 'application/json',
+      }),
     };
     return valores;
   }
-  
-  login(credenciales:any): Observable<any> {
+
+  login(credenciales: any): Observable<any> {
     const httpOptions = this.getOptions();
-    return this.http.post(this.url + '/signin', credenciales, httpOptions).pipe(map(data=>{
-    sessionStorage.setItem('currenUser', JSON.stringify(data));
-    this.currentUserSubject.next(data);
-    return data;
-    }))
+    return this.http.post(this.url + '/signin', credenciales, httpOptions).pipe(
+      map((data) => {
+        sessionStorage.setItem('currenUser', JSON.stringify(data));
+        this.currentUserSubject.next(data);
+        return data;
+      })
+    );
   }
 
   register(username: string, email: string, password: string): Observable<any> {
     const httpOptions = this.getOptions();
-    return this.http.post(this.url + '/signup', {
-      username,
-      email,
-      password
-    }, httpOptions);
+    return this.http.post(
+      this.url + '/signup',
+      {
+        username,
+        email,
+        password,
+      },
+      httpOptions
+    );
   }
-
 }
